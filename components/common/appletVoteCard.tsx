adefe81bb888038a4abaadbb642b03189dd10fcb
Yomi Ikuru <captyomjnr@gmail.com>
2023-09-03 22:08:01 +0100

    Fix styling for applet vote card

--- components/common/appletVoteCard.tsx
+++ components/common/appletVoteCard.tsx
@@ -139,12 +139,10 @@ export const AppletVoteCard: React.FC<{ otherApplets: string[] }> = ({
 
 const SCAppletVoteCard = styled(YStack, {
   minHeight: "$16",
-  width: "100%",
   maxWidth: "$30",
   flex: 1,
-  alignSelf: "center",
   marginTop: "$3",
-  marginHorizontal: "$3",
+  marginHorizontal: "$2",
   marginBottom: "$3",
   backgroundColor: "$gray1",
   borderRadius: "$8",
