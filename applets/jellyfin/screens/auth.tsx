860d3b7b6ed1f21f9c3254c2a73fa380e7d2979f
Yomi Ikuru <captyomjnr@gmail.com>
2023-09-21 22:50:28 +0100

    Fix paste issue on auth page by using KeyboardAvoidingScrollView

--- applets/jellyfin/screens/auth.tsx
+++ applets/jellyfin/screens/auth.tsx
@@ -1,14 +1,6 @@
 import React from "react";
-import {
-  ScrollView,
-  Button,
-  Form,
-  Text,
-  Input,
-  YStack,
-  XStack,
-  Spinner,
-} from "tamagui";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Button, Form, Text, Input, YStack, XStack, Spinner } from "tamagui";
 import { Ionicons } from "@expo/vector-icons";
 
 import { useTranslation } from "react-i18next";
@@ -131,215 +123,214 @@ const JellyfinAuth = () => {
   }, [customHeaders]);
 
   return (
-    <ScrollView
-      height="100%"
-      // keyboardDismissMode="on-drag"
-      keyboardShouldPersistTaps="handled"
-      automaticallyAdjustKeyboardInsets={true}
-    >
-      <AppletButtonBanner applet={Applets.JELLYFIN} />
-      <Form
-        space
-        width="100%"
-        paddingHorizontal="$4"
-        paddingVertical="$4"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <YStack>
-          <XStack paddingBottom="$2" marginLeft="$1" alignItems="flex-end">
-            <Text color="$color" fontSize={16}>
-              {t("common:serverURL")}
-            </Text>
-            <Text color="$red9" marginLeft="$2">
-              {errors.serverURL && errors.serverURL.message}
-            </Text>
-          </XStack>
-          <Controller
-            name="serverURL"
-            control={control}
-            rules={{
-              required: true,
-              validate: (value) =>
-                !!UrlRegexPattern.test(value) ||
-                (t("common:invalidURL") as string),
-            }}
-            render={({ field: { onChange, value } }) => (
-              <Input
-                size="$4"
-                width="100%"
-                borderWidth={2}
-                borderColor={errors.serverURL ? "$red8" : "$gray6"}
-                placeholder={`${t("common:serverURL")}...`}
-                onChangeText={onChange}
-                value={value}
-                {...register("serverURL", {
-                  required: t("common:required") as string,
-                })}
-              />
-            )}
-          />
-        </YStack>
-        <YStack>
-          <XStack paddingBottom="$2" marginLeft="$1" alignItems="flex-end">
-            <Text color="$color" fontSize={16}>
-              {t("common:username")}
-            </Text>
-            <Text color="$red9" marginLeft="$2">
-              {errors.username && errors.username.message}
-            </Text>
-          </XStack>
-          <Controller
-            name="username"
-            control={control}
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, value } }) => (
-              <Input
-                size="$4"
-                width="100%"
-                borderWidth={2}
-                borderColor={errors.username ? "$red8" : "$gray6"}
-                placeholder={`${t("common:username")}...`}
-                onChangeText={onChange}
-                value={value}
-                {...register("username", {
-                  required: t("common:required") as string,
-                })}
-              />
-            )}
-          />
-        </YStack>
-        <YStack>
-          <XStack paddingBottom="$2" marginLeft="$1" alignItems="flex-end">
-            <Text color="$color" fontSize={16}>
-              {t("common:password")}
-            </Text>
-            <Text color="$red9" marginLeft="$2">
-              {errors.password && errors.password.message}
-            </Text>
-          </XStack>
-          <Controller
-            name="password"
-            control={control}
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, value } }) => (
-              <XStack width="100%">
+    <YStack height="100%">
+      <KeyboardAwareScrollView>
+        <AppletButtonBanner applet={Applets.JELLYFIN} />
+        <Form
+          space
+          width="100%"
+          paddingHorizontal="$4"
+          paddingVertical="$4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <YStack>
+            <XStack paddingBottom="$2" marginLeft="$1" alignItems="flex-end">
+              <Text color="$color" fontSize={16}>
+                {t("common:serverURL")}
+              </Text>
+              <Text color="$red9" marginLeft="$2">
+                {errors.serverURL && errors.serverURL.message}
+              </Text>
+            </XStack>
+            <Controller
+              name="serverURL"
+              control={control}
+              rules={{
+                required: true,
+                validate: (value) =>
+                  !!UrlRegexPattern.test(value) ||
+                  (t("common:invalidURL") as string),
+              }}
+              render={({ field: { onChange, value } }) => (
                 <Input
-                  flex={1}
                   size="$4"
+                  width="100%"
                   borderWidth={2}
-                  borderColor={errors.password ? "$red8" : "$gray6"}
-                  placeholder={`${t("common:password")}...`}
-                  textContentType="password"
-                  secureTextEntry={!showPassword}
+                  borderColor={errors.serverURL ? "$red8" : "$gray6"}
+                  placeholder={`${t("common:serverURL")}...`}
                   onChangeText={onChange}
                   value={value}
-                  {...register("password", {
+                  {...register("serverURL", {
                     required: t("common:required") as string,
                   })}
                 />
-                <Button
-                  width="$4"
-                  marginLeft="$1.5"
-                  borderWidth={2}
-                  borderColor="$gray6"
-                  padding="$0"
-                  backgroundColor={showPassword ? "$gray8" : "$gray1"}
-                  onPress={() => setShowPassword(!showPassword)}
-                >
-                  <Ionicons
-                    name={showPassword ? "eye" : "eye-off"}
-                    size={24}
-                    color={iconColor}
-                  />
-                </Button>
-              </XStack>
-            )}
-          />
-        </YStack>
-        <Form.Trigger asChild>
-          <Button theme="blue" width="100%">
-            <XStack flex={1} alignItems="center" justifyContent="center">
-              <Text>
-                {auth.status === "loading" ? <Spinner /> : t("common:signIn")}
+              )}
+            />
+          </YStack>
+          <YStack>
+            <XStack paddingBottom="$2" marginLeft="$1" alignItems="flex-end">
+              <Text color="$color" fontSize={16}>
+                {t("common:username")}
+              </Text>
+              <Text color="$red9" marginLeft="$2">
+                {errors.username && errors.username.message}
               </Text>
             </XStack>
-          </Button>
-        </Form.Trigger>
-        <YStack>
-          <Button
-            theme="dark"
-            backgroundColor="$gray6"
-            onPress={() => {
-              Alert.prompt(
-                t("common:addCustomHeader"),
-                t("common:addCustomHeader_description") as string,
-                [
-                  { text: t("common:cancel") as string, onPress: () => { } },
-                  {
-                    text: t("common:add") as string,
-                    onPress: (value) => {
-                      if (value && validateCustomHeader(value))
-                        setCustomHeaders([
-                          ...customHeaders,
-                          {
-                            [value.split(":", 1)[0]]: value
-                              .split(":")
-                              .slice(1)
-                              .join(":"),
-                          },
-                        ]);
-                      else
-                        Alert.alert(
-                          t("common:invalidCustomHeader"),
-                          t("common:invalidCustomHeader_description") as string
-                        );
-                    },
-                  },
-                ],
-                "plain-text"
-              );
-            }}
-          >
-            {t("common:addCustomHeaders")}
-          </Button>
-        </YStack>
-      </Form>
-      <XStack width="100%" height="$4">
-        <FlashList
-          horizontal
-          data={customHeaders}
-          renderItem={({ item, index }) => (
+            <Controller
+              name="username"
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  size="$4"
+                  width="100%"
+                  borderWidth={2}
+                  borderColor={errors.username ? "$red8" : "$gray6"}
+                  placeholder={`${t("common:username")}...`}
+                  onChangeText={onChange}
+                  value={value}
+                  {...register("username", {
+                    required: t("common:required") as string,
+                  })}
+                />
+              )}
+            />
+          </YStack>
+          <YStack>
+            <XStack paddingBottom="$2" marginLeft="$1" alignItems="flex-end">
+              <Text color="$color" fontSize={16}>
+                {t("common:password")}
+              </Text>
+              <Text color="$red9" marginLeft="$2">
+                {errors.password && errors.password.message}
+              </Text>
+            </XStack>
+            <Controller
+              name="password"
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, value } }) => (
+                <XStack width="100%">
+                  <Input
+                    flex={1}
+                    size="$4"
+                    borderWidth={2}
+                    borderColor={errors.password ? "$red8" : "$gray6"}
+                    placeholder={`${t("common:password")}...`}
+                    textContentType="password"
+                    secureTextEntry={!showPassword}
+                    onChangeText={onChange}
+                    value={value}
+                    {...register("password", {
+                      required: t("common:required") as string,
+                    })}
+                  />
+                  <Button
+                    width="$4"
+                    marginLeft="$1.5"
+                    borderWidth={2}
+                    borderColor="$gray6"
+                    padding="$0"
+                    backgroundColor={showPassword ? "$gray8" : "$gray1"}
+                    onPress={() => setShowPassword(!showPassword)}
+                  >
+                    <Ionicons
+                      name={showPassword ? "eye" : "eye-off"}
+                      size={24}
+                      color={iconColor}
+                    />
+                  </Button>
+                </XStack>
+              )}
+            />
+          </YStack>
+          <Form.Trigger asChild>
+            <Button theme="blue" width="100%">
+              <XStack flex={1} alignItems="center" justifyContent="center">
+                <Text>
+                  {auth.status === "loading" ? <Spinner /> : t("common:signIn")}
+                </Text>
+              </XStack>
+            </Button>
+          </Form.Trigger>
+          <YStack>
             <Button
               theme="dark"
               backgroundColor="$gray6"
-              width="$10"
-              height="$2"
-              marginRight="$2"
-              justifyContent="space-between"
-              paddingHorizontal="$3"
-              alignItems="center"
-              borderRadius="$6"
               onPress={() => {
-                setCustomHeaders(
-                  customHeaders.filter((_, idx) => idx !== index)
+                Alert.prompt(
+                  t("common:addCustomHeader"),
+                  t("common:addCustomHeader_description") as string,
+                  [
+                    { text: t("common:cancel") as string, onPress: () => {} },
+                    {
+                      text: t("common:add") as string,
+                      onPress: (value) => {
+                        if (value && validateCustomHeader(value))
+                          setCustomHeaders([
+                            ...customHeaders,
+                            {
+                              [value.split(":", 1)[0]]: value
+                                .split(":")
+                                .slice(1)
+                                .join(":"),
+                            },
+                          ]);
+                        else
+                          Alert.alert(
+                            t("common:invalidCustomHeader"),
+                            t(
+                              "common:invalidCustomHeader_description"
+                            ) as string
+                          );
+                      },
+                    },
+                  ],
+                  "plain-text"
                 );
               }}
             >
-              <Text width="75%" ellipsizeMode="tail" numberOfLines={1}>
-                {Object.keys(item)[0]}
-              </Text>
-              <Ionicons name="close" size={16} color="red" />
+              {t("common:addCustomHeaders")}
             </Button>
-          )}
-          ListHeaderComponent={<XStack marginLeft="$4" />}
-          estimatedItemSize={74}
-        />
-      </XStack>
-    </ScrollView>
+          </YStack>
+        </Form>
+        <XStack width="100%" height="$4">
+          <FlashList
+            horizontal
+            data={customHeaders}
+            renderItem={({ item, index }) => (
+              <Button
+                theme="dark"
+                backgroundColor="$gray6"
+                width="$10"
+                height="$2"
+                marginRight="$2"
+                justifyContent="space-between"
+                paddingHorizontal="$3"
+                alignItems="center"
+                borderRadius="$6"
+                onPress={() => {
+                  setCustomHeaders(
+                    customHeaders.filter((_, idx) => idx !== index)
+                  );
+                }}
+              >
+                <Text width="75%" ellipsizeMode="tail" numberOfLines={1}>
+                  {Object.keys(item)[0]}
+                </Text>
+                <Ionicons name="close" size={16} color="red" />
+              </Button>
+            )}
+            ListHeaderComponent={<XStack marginLeft="$4" />}
+            estimatedItemSize={74}
+          />
+        </XStack>
+      </KeyboardAwareScrollView>
+    </YStack>
   );
 };
 
--- applets/radarr/screens/auth.tsx
+++ applets/radarr/screens/auth.tsx
@@ -1,14 +1,6 @@
 import React from "react";
-import {
-  ScrollView,
-  Button,
-  Form,
-  Text,
-  Input,
-  YStack,
-  XStack,
-  Spinner,
-} from "tamagui";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Button, Form, Text, Input, YStack, XStack, Spinner } from "tamagui";
 import { Ionicons } from "@expo/vector-icons";
 
 import { useTranslation } from "react-i18next";
@@ -114,187 +106,186 @@ const RadarrAuth = () => {
   }, [customHeaders]);
 
   return (
-    <ScrollView
-      height="100%"
-      // keyboardDismissMode="on-drag"
-      keyboardShouldPersistTaps="handled"
-      automaticallyAdjustKeyboardInsets={true}
-    >
-      <AppletButtonBanner applet={Applets.RADARR} />
-      <Form
-        space
-        width="100%"
-        paddingHorizontal="$4"
-        paddingVertical="$4"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <YStack>
-          <XStack paddingBottom="$2" marginLeft="$1" alignItems="flex-end">
-            <Text color="$color" fontSize={16}>
-              {t("common:serverURL")}
-            </Text>
-            <Text color="$red9" marginLeft="$2">
-              {errors.serverURL && errors.serverURL.message}
-            </Text>
-          </XStack>
-          <Controller
-            name="serverURL"
-            control={control}
-            rules={{
-              validate: (value) =>
-                !!UrlRegexPattern.test(value) ||
-                (t("common:invalidURL") as string),
-            }}
-            render={({ field: { onChange, value } }) => (
-              <Input
-                size="$4"
-                width="100%"
-                borderWidth={2}
-                borderColor={errors.serverURL ? "$red8" : "$gray6"}
-                placeholder={`${t("common:serverURL")}...`}
-                onChangeText={onChange}
-                value={value}
-                {...register("serverURL", {
-                  required: t("common:required") as string,
-                })}
-              />
-            )}
-          />
-        </YStack>
-        <YStack>
-          <XStack paddingBottom="$2" marginLeft="$1" alignItems="flex-end">
-            <Text color="$color" fontSize={16}>
-              {t("common:apiKey")}
-            </Text>
-            <Text color="$red9" marginLeft="$2">
-              {errors.apiKey && errors.apiKey.message}
-            </Text>
-          </XStack>
-          <Controller
-            name="apiKey"
-            control={control}
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, value } }) => (
-              <XStack width="100%">
+    <YStack height="100%">
+      <KeyboardAwareScrollView keyboardShouldPersistTaps="handled">
+        <AppletButtonBanner applet={Applets.RADARR} />
+        <Form
+          space
+          width="100%"
+          paddingHorizontal="$4"
+          paddingVertical="$4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <YStack>
+            <XStack paddingBottom="$2" marginLeft="$1" alignItems="flex-end">
+              <Text color="$color" fontSize={16}>
+                {t("common:serverURL")}
+              </Text>
+              <Text color="$red9" marginLeft="$2">
+                {errors.serverURL && errors.serverURL.message}
+              </Text>
+            </XStack>
+            <Controller
+              name="serverURL"
+              control={control}
+              rules={{
+                validate: (value) =>
+                  !!UrlRegexPattern.test(value) ||
+                  (t("common:invalidURL") as string),
+              }}
+              render={({ field: { onChange, value } }) => (
                 <Input
-                  flex={1}
                   size="$4"
+                  width="100%"
                   borderWidth={2}
-                  borderColor={errors.apiKey ? "$red8" : "$gray6"}
-                  placeholder={`${t("common:apiKey")}...`}
-                  textContentType="password"
-                  secureTextEntry={!showApiKey}
+                  borderColor={errors.serverURL ? "$red8" : "$gray6"}
+                  placeholder={`${t("common:serverURL")}...`}
                   onChangeText={onChange}
                   value={value}
-                  {...register("apiKey", {
+                  {...register("serverURL", {
                     required: t("common:required") as string,
                   })}
                 />
-                <Button
-                  width="$4"
-                  marginLeft="$1.5"
-                  borderWidth={2}
-                  borderColor="$gray6"
-                  padding="$0"
-                  backgroundColor={showApiKey ? "$gray8" : "$gray1"}
-                  onPress={() => setShowApiKey(!showApiKey)}
-                >
-                  <Ionicons
-                    name={showApiKey ? "eye" : "eye-off"}
-                    size={24}
-                    color={iconColor}
-                  />
-                </Button>
-              </XStack>
-            )}
-          />
-        </YStack>
-        <Form.Trigger asChild>
-          <Button theme="blue" width="100%">
-            <XStack flex={1} alignItems="center" justifyContent="center">
-              <Text>
-                {auth.fetchStatus === "fetching" ? (
-                  <Spinner />
-                ) : (
-                  t("common:signIn")
-                )}
+              )}
+            />
+          </YStack>
+          <YStack>
+            <XStack paddingBottom="$2" marginLeft="$1" alignItems="flex-end">
+              <Text color="$color" fontSize={16}>
+                {t("common:apiKey")}
+              </Text>
+              <Text color="$red9" marginLeft="$2">
+                {errors.apiKey && errors.apiKey.message}
               </Text>
             </XStack>
-          </Button>
-        </Form.Trigger>
-        <YStack>
-          <Button
-            theme="dark"
-            backgroundColor="$gray6"
-            onPress={() => {
-              Alert.prompt(
-                t("common:addCustomHeader"),
-                t("common:addCustomHeader_description") as string,
-                [
-                  { text: t("common:cancel") as string, onPress: () => { } },
-                  {
-                    text: t("common:add") as string,
-                    onPress: (value) => {
-                      if (value && validateCustomHeader(value))
-                        setCustomHeaders([
-                          ...customHeaders,
-                          {
-                            [value.split(":", 1)[0]]: value
-                              .split(":")
-                              .slice(1)
-                              .join(":"),
-                          },
-                        ]);
-                      else
-                        Alert.alert(
-                          t("common:invalidCustomHeader"),
-                          t("common:invalidCustomHeader_description") as string
-                        );
-                    },
-                  },
-                ],
-                "plain-text"
-              );
-            }}
-          >
-            {t("common:addCustomHeaders")}
-          </Button>
-        </YStack>
-      </Form>
-      <XStack width="100%" height="$4">
-        <FlashList
-          horizontal
-          data={customHeaders}
-          renderItem={({ item, index }) => (
+            <Controller
+              name="apiKey"
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, value } }) => (
+                <XStack width="100%">
+                  <Input
+                    flex={1}
+                    size="$4"
+                    borderWidth={2}
+                    borderColor={errors.apiKey ? "$red8" : "$gray6"}
+                    placeholder={`${t("common:apiKey")}...`}
+                    textContentType="password"
+                    secureTextEntry={!showApiKey}
+                    onChangeText={onChange}
+                    value={value}
+                    {...register("apiKey", {
+                      required: t("common:required") as string,
+                    })}
+                  />
+                  <Button
+                    width="$4"
+                    marginLeft="$1.5"
+                    borderWidth={2}
+                    borderColor="$gray6"
+                    padding="$0"
+                    backgroundColor={showApiKey ? "$gray8" : "$gray1"}
+                    onPress={() => setShowApiKey(!showApiKey)}
+                  >
+                    <Ionicons
+                      name={showApiKey ? "eye" : "eye-off"}
+                      size={24}
+                      color={iconColor}
+                    />
+                  </Button>
+                </XStack>
+              )}
+            />
+          </YStack>
+          <Form.Trigger asChild>
+            <Button theme="blue" width="100%">
+              <XStack flex={1} alignItems="center" justifyContent="center">
+                <Text>
+                  {auth.fetchStatus === "fetching" ? (
+                    <Spinner />
+                  ) : (
+                    t("common:signIn")
+                  )}
+                </Text>
+              </XStack>
+            </Button>
+          </Form.Trigger>
+          <YStack>
             <Button
               theme="dark"
               backgroundColor="$gray6"
-              width="$10"
-              height="$2"
-              marginRight="$2"
-              justifyContent="space-between"
-              paddingHorizontal="$3"
-              alignItems="center"
-              borderRadius="$6"
               onPress={() => {
-                setCustomHeaders(
-                  customHeaders.filter((_, idx) => idx !== index)
+                Alert.prompt(
+                  t("common:addCustomHeader"),
+                  t("common:addCustomHeader_description") as string,
+                  [
+                    { text: t("common:cancel") as string, onPress: () => {} },
+                    {
+                      text: t("common:add") as string,
+                      onPress: (value) => {
+                        if (value && validateCustomHeader(value))
+                          setCustomHeaders([
+                            ...customHeaders,
+                            {
+                              [value.split(":", 1)[0]]: value
+                                .split(":")
+                                .slice(1)
+                                .join(":"),
+                            },
+                          ]);
+                        else
+                          Alert.alert(
+                            t("common:invalidCustomHeader"),
+                            t(
+                              "common:invalidCustomHeader_description"
+                            ) as string
+                          );
+                      },
+                    },
+                  ],
+                  "plain-text"
                 );
               }}
             >
-              <Text width="75%" ellipsizeMode="tail" numberOfLines={1}>
-                {Object.keys(item)[0]}
-              </Text>
-              <Ionicons name="close" size={16} color="red" />
+              {t("common:addCustomHeaders")}
             </Button>
-          )}
-          ListHeaderComponent={<XStack marginLeft="$4" />}
-          estimatedItemSize={74}
-        />
-      </XStack>
-    </ScrollView>
+          </YStack>
+        </Form>
+        <XStack width="100%" height="$4">
+          <FlashList
+            horizontal
+            data={customHeaders}
+            renderItem={({ item, index }) => (
+              <Button
+                theme="dark"
+                backgroundColor="$gray6"
+                width="$10"
+                height="$2"
+                marginRight="$2"
+                justifyContent="space-between"
+                paddingHorizontal="$3"
+                alignItems="center"
+                borderRadius="$6"
+                onPress={() => {
+                  setCustomHeaders(
+                    customHeaders.filter((_, idx) => idx !== index)
+                  );
+                }}
+              >
+                <Text width="75%" ellipsizeMode="tail" numberOfLines={1}>
+                  {Object.keys(item)[0]}
+                </Text>
+                <Ionicons name="close" size={16} color="red" />
+              </Button>
+            )}
+            ListHeaderComponent={<XStack marginLeft="$4" />}
+            estimatedItemSize={74}
+          />
+        </XStack>
+      </KeyboardAwareScrollView>
+    </YStack>
   );
 };
 
