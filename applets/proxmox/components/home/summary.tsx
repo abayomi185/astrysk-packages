41648734b8a98af33d0d3f425cbc6ec1ab1eef7d
Yomi Ikuru <captyomjnr@gmail.com>
2023-10-14 23:42:04 +0100

    add: Skeleton for summary (home) screen

---
+++ applets/proxmox/components/home/summary.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useRouter } from "expo-router";
+import { RefreshControl } from "react-native";
+import { Image, ImageSource } from "expo-image";
+import { YStack, Text, useTheme, XStack, H5, Button } from "tamagui";
+import { useLoadingSpinner } from "@astrysk/utils";
+import { EmptyList, SectionTitle } from "@astrysk/components";
+import { useTranslation } from "react-i18next";
+import { useProxmoxStore } from "../../store";
+import { ProxmoxDetailScreenContext } from "../../types";
+import { Actions } from "@astrysk/constants";
+import { TabContext } from "@astrysk/types";
+import { FlashList } from "@shopify/flash-list";
+
+const ProxmoxSummary: React.FC = () => {
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  const baseURL = useProxmoxStore.getState().baseURL as string;
+  const token = useProxmoxStore.getState().token as string;
+
+  useLoadingSpinner(ProxmoxSummary.name);
+
+  return (
+    <YStack flex={1}>
+      <FlashList data={[]} renderItem={({ item, index }) => <></>} />
+    </YStack>
+  );
+};
+
+export default ProxmoxSummary;
