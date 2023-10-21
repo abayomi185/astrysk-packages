fcf287e73773b7c77d3c863ad6eca61138edb60e
Yomi Ikuru <captyomjnr@gmail.com>
2023-10-21 23:39:00 +0100

    add: proxmox action panel scaffold

---
+++ applets/proxmox/components/detail/actionPanel.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { GetClusterResourcesResponseResponseDataItem } from "../../api";
+
+const ProxmoxActionPanel: React.FC<{
+  data: GetClusterResourcesResponseResponseDataItem;
+}> = () => {
+  return <></>;
+};
+
+export default ProxmoxActionPanel;
