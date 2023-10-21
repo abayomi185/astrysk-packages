cf7691ab2da161ba88feb569564a77eeb5a47988
Yomi Ikuru <captyomjnr@gmail.com>
2023-10-21 23:33:27 +0100

    update: proxmox locales

--- applets/proxmox/locales.ts
+++ applets/proxmox/locales.ts
@@ -1,5 +1,33 @@
-const lidarrLocales = {
+const proxmoxLocales = {
+  qemu: "QEMU",
+  lxc: "LXC",
+  storage: "Storage",
+  node: "Node",
+  sdn: "SDN",
+
+  maxcpu: "Max CPU",
+  maxmemory: "Max Memory",
+
+  gb: "GB",
+
+  status: "Status",
   deleteCache: "Delete Cache",
+
+  online: "Online",
+  uptime: "Uptime",
+  template: "Template",
+
+  type: "Type",
+  order: "Order",
+
+  id: "ID",
+  alphabetical: "Alphabetical",
+
+  running: "Running",
+  stopped: "Stopped",
+  paused: "Paused",
+
+  noResourcesFound: "No Resources Found",
 };
 
-export default lidarrLocales;
+export default proxmoxLocales;
