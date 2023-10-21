5f5f888d74741132d5ebaf6acaa144b21e27f931
Yomi Ikuru <captyomjnr@gmail.com>
2023-10-21 23:38:41 +0100

    add: promox summary screen

--- applets/proxmox/components/home/summary.tsx
+++ applets/proxmox/components/home/summary.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "expo-router";
 import { RefreshControl } from "react-native";
 import { Image, ImageSource } from "expo-image";
 import { YStack, Text, useTheme, XStack, H5, Button } from "tamagui";
-import { useLoadingSpinner } from "@astrysk/utils";
+import { useQueryLoadingSpinner } from "@astrysk/utils";
 import { EmptyList, SectionTitle } from "@astrysk/components";
 import { useTranslation } from "react-i18next";
 import { useProxmoxStore } from "../../store";
@@ -11,7 +11,13 @@ import { ProxmoxDetailScreenContext } from "../../types";
 import { Actions } from "@astrysk/constants";
 import { TabContext } from "@astrysk/types";
 import { FlashList } from "@shopify/flash-list";
-import { useGetClusterResources } from "../../api";
+import {
+  GetClusterResourcesResponseResponseDataItem,
+  useGetClusterResources,
+  useGetClusterStatus,
+} from "../../api";
+import { proxmoxColors } from "../../colors";
+import { ProxmoxSummaryCharts } from "../detail/charts";
 
 const ProxmoxSummary: React.FC = () => {
   const { t } = useTranslation();
@@ -20,18 +26,45 @@ const ProxmoxSummary: React.FC = () => {
   const baseURL = useProxmoxStore.getState().baseURL as string;
   const token = useProxmoxStore.getState().token as string;
 
-  const clusterResources = useGetClusterResources({
+  const clusterStatus = useGetClusterStatus({
     query: {
-      onSuccess: (data) => {},
-      onError: (error) => {},
+      select: (response) => response.data,
     },
   });
 
-  useLoadingSpinner(ProxmoxSummary.name);
+  useQueryLoadingSpinner(clusterStatus);
 
   return (
     <YStack flex={1}>
-      <FlashList data={[]} renderItem={({ item, index }) => <></>} />
+      <FlashList
+        data={[]}
+        renderItem={({ item, index }) => <ProxmoxSummaryCharts data={item} />}
+        extraData={new Date()}
+        estimatedItemSize={100}
+        ListHeaderComponent={
+          <YStack flex={1} height="$8" backgroundColor="green">
+            <FlashList
+              horizontal={true}
+              data={clusterStatus.data}
+              extraData={new Date()}
+              renderItem={({ item }) => (
+                <Button
+                  flex={1}
+                  width="$13"
+                  marginVertical="$2"
+                  backgroundColor="green"
+                >
+                  <XStack flex={1} justifyContent="center" alignItems="center">
+                    <Text color="white">{item?.name}</Text>
+                  </XStack>
+                </Button>
+              )}
+              estimatedItemSize={100}
+              ListHeaderComponent={<XStack width="$0.75" />}
+            />
+          </YStack>
+        }
+      />
     </YStack>
   );
 };
