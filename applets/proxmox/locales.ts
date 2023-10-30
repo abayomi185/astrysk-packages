7bebd4dff836a7cfd5f15ef8d61e8f8b80973d2b
Yomi Ikuru <captyomjnr@gmail.com>
2023-10-30 23:31:59 +0000

    update: Proxmox locales

--- applets/proxmox/locales.ts
+++ applets/proxmox/locales.ts
@@ -15,13 +15,36 @@ const proxmoxLocales = {
   memory: "Memory",
   diskSpace: "Disk Space",
   diskSpaceRootFs: "Disk Space (Root FS)",
+  disk: "Disk",
   rootFs: "Root FS",
+  filesystem: "File System",
   swap: "Swap",
   kveversion: "KVE Version",
   pveversion: "PVE Version",
+
+  // Chart legends
+  cpuUsage: "CPU Usage",
+  serverLoad: "Server Load",
+  loadavg: "Load Avg",
+  memoryUsage: "Memory Usage",
+  storageUsage: "Storage Usage",
+  mem: "Memory",
+  memused: "Memory Used",
+  memtotal: "Memory Total",
+  maxmem: "Max Memory",
+  swapUsage: "Swap Usage",
+  swapused: "Swap Used",
+  swaptotal: "Swap Total",
+  networkTraffic: "Network Traffic",
+  netin: "Net In",
+  netout: "Net Out",
+  "day(avg)": "Day (Avg)",
+
   gb: "GB",
 
+  usage: "Usage",
   status: "Status",
+  content: "Content",
   deleteCache: "Delete Cache",
 
   online: "Online",
@@ -39,6 +62,8 @@ const proxmoxLocales = {
   running: "Running",
   stopped: "Stopped",
   paused: "Paused",
+  suspended: "Suspended",
+  available: "Available",
 
   noResourcesFound: "No Resources Found",
 };
