211d168b578c5d6b4027b5432a28e1ca70652c1d
Yomi Ikuru <captyomjnr@gmail.com>
2024-01-01 10:03:51 +0000

    add: new ollama colors

--- applets/ollama/colors.ts
+++ applets/ollama/colors.ts
@@ -3,8 +3,12 @@ import { AppletColors } from "@astrysk/types";
 // Ollama colors
 export const ollamaColors: AppletColors = {
   primary: "$gray8",
+  primaryColorDark: "#FFFFFF",
+  primaryColorLight: "#343541",
   secondary: "$blue5",
   accentColor: "#343541",
+  accentColorDark: "#FFFFFF",
+  accentColorLight: "#343541",
   activeCheckFillColor: "#9fdfc3",
   activeBookmarkFillColor: "#e4aaae",
   bannerBackground: "#432C16",
