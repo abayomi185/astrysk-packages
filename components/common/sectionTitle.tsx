e3670a7ddfdf3c5e818406eec7ca8f701c2845e7
Yomi Ikuru <captyomjnr@gmail.com>
2024-01-01 10:02:10 +0000

    fix: Disable selection from sectionTitle component

--- components/common/sectionTitle.tsx
+++ components/common/sectionTitle.tsx
@@ -3,6 +3,7 @@ import { styled, H3 } from "tamagui";
 export const SectionTitle = styled(H3, {
   paddingVertical: "$2",
   paddingHorizontal: "$3",
+  selectable: false,
   variants: {
     subtle: {
       true: {
