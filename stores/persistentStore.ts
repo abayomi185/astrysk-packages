3ca50a0a87366b6446f72be687969d89b192f6a5
Yomi Ikuru <captyomjnr@gmail.com>
2024-10-27 19:18:32 +0000

    chore: comment out unused code

--- stores/persistentStore.ts
+++ stores/persistentStore.ts
@@ -39,37 +39,37 @@ const mmkvStore = new MMKV({
 
 // export default PersistentAppStore;
 
-// WARN: Not using AsyncStorage - using MMKV
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-const PersistentAppStateStore: PersistStorage<AppState> = {
-  getItem: async (name: string) => {
-    // const data = await AsyncStorage.getItem(name);
-    const data = mmkvStore.getString(name);
-    if (data === undefined) return null;
-    const appState = JSON.parse(data) as AppState;
-    return {
-      state: {
-        ...appState,
-        componentsLoading: new Set(appState.componentsLoading),
-      },
-      version: APP_STATE_VERSION,
-    } as StorageValue<AppState>;
-  },
-  setItem: async (name: string, value: StorageValue<AppState>) => {
-    // const data await AsyncStorage.setItem(name, value)
-    const data: StorageValue<AppState> = {
-      ...value,
-      state: {
-        ...value.state,
-        // @ts-ignore
-        // Custom stringify for componentsLoading Set
-        componentsLoading: Array.from(value.state.componentsLoading),
-      },
-    };
-    await AsyncStorage.setItem(name, JSON.stringify(data));
-  },
-  removeItem: async (name: string) => {
-    await AsyncStorage.removeItem(name);
-  },
-};
+// // WARN: Not using AsyncStorage - using MMKV
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+//
+// const PersistentAppStateStore: PersistStorage<AppState> = {
+//   getItem: async (name: string) => {
+//     // const data = await AsyncStorage.getItem(name);
+//     const data = mmkvStore.getString(name);
+//     if (data === undefined) return null;
+//     const appState = JSON.parse(data) as AppState;
+//     return {
+//       state: {
+//         ...appState,
+//         componentsLoading: new Set(appState.componentsLoading),
+//       },
+//       version: APP_STATE_VERSION,
+//     } as StorageValue<AppState>;
+//   },
+//   setItem: async (name: string, value: StorageValue<AppState>) => {
+//     // const data await AsyncStorage.setItem(name, value)
+//     const data: StorageValue<AppState> = {
+//       ...value,
+//       state: {
+//         ...value.state,
+//         // @ts-ignore
+//         // Custom stringify for componentsLoading Set
+//         componentsLoading: Array.from(value.state.componentsLoading),
+//       },
+//     };
+//     await AsyncStorage.setItem(name, JSON.stringify(data));
+//   },
+//   removeItem: async (name: string) => {
+//     await AsyncStorage.removeItem(name);
+//   },
+// };
